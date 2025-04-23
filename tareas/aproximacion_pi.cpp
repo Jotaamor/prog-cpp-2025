1b3a8dd89caaa660df23e74524636d43f30eb605
Jotaamor <jozef4516@gmail.com>
2025-04-22 22:48:31 -0500

    error en el calculo y correccion general

--- tareas/aproximacion_pi.cpp
+++ tareas/aproximacion_pi.cpp
@@ -4,10 +4,12 @@
 void pi_aprox(int n);
 int n;
 
+//
+
 int main (void){
-    int n=0;
-    std::cout << "¿Cuantos decimales de pi exactos necesita?\n";
-    std::cin >> n;
+    int n=20;
+    //std::cout << "¿Cuantos decimales de pi exactos necesita?\n";
+    //std::cin >> n;
     pi_aprox(n);
 
     return 0;
@@ -17,14 +19,16 @@ void pi_aprox(int n){
     int N=n;
     int k=0;
     long double sum_pi = 0; 
+    long double error = 0;
 
-    while (k!=N){
+    while (k!=N+1){
         sum_pi=sum_pi+((1/pow(16, k))*((4.0/((8*k)+1))-(2.0/((8*k)+4))-(1.0/((8*k)+5))-(1.0/((8*k)+6))));
-        std::cout << std::setprecision(k+3);//setprecision: quita el limite de salida de decimales en la pantalla
-        //k+3 es suficiente para que no se aproxime el decimal importante
-        //std::cout <<"pi es igual a:"<< sum_pi <<"\n"; 
+        std::cout << std::setprecision(20);//setprecision: quita el limite de salida de decimales en la pantalla
+        std::cout.setf(std::ios::scientific);
+        error=fabs(1-sum_pi/M_PI);
+        std::cout <<"n:"<< k <<"\n"; 
+        std::cout <<"pi:"<< sum_pi; 
+        std::cout <<" el error es de:"<< error<<"\n";
         k=k+1;
-        //std::cout <<"Cuenta en:"<< k <<"\n"; 
     }
-    std::cout <<"pi es igual a: "<< sum_pi <<"\n"; 
 }
